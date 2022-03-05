de04a987d9df331ab6622757ae962746af51ce51
SnowCait <SnowCait@users.noreply.github.com>
2022-03-05 17:05:55 +0900

    Status additional information

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import {approved} from './approved'
 async function run(): Promise<void> {
   try {
     const token: string = core.getInput('token')
+    const statusContext: string = core.getInput('status-context')
+    const statusDescription: string = core.getInput('status-description')
 
     core.debug(JSON.stringify(github.context))
 
@@ -13,7 +15,9 @@ async function run(): Promise<void> {
       ...github.context.repo,
       sha: github.context.payload.pull_request?.head.sha,
       state: (await approved(token)) ? 'success' : 'failure',
-      context: 'review passing'
+      context: statusContext,
+      description: statusDescription,
+      target_url: `${github.context.serverUrl}/${github.context.repo.owner}/${github.context.repo.repo}/actions/runs/${github.context.runId}`
     })
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
