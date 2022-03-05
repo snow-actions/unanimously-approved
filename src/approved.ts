096df83e4d8563512422f98ac149675ad8b8f2cf
SnowCait <SnowCait@users.noreply.github.com>
2022-03-05 15:38:18 +0900

    `review passing` status

---
+++ src/approved.ts
@@ -0,0 +1,58 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import {
+  PullRequestEvent,
+  PullRequestReviewEvent
+} from '@octokit/webhooks-definitions/schema'
+
+export async function approved(token: string): Promise<boolean> {
+  const {pull_request: pr} =
+    github.context.eventName == 'pull_request'
+      ? (github.context.payload as PullRequestEvent)
+      : (github.context.payload as PullRequestReviewEvent)
+
+  core.debug(`PR#${pr.number}`)
+  core.debug(`requested reviewers: ${pr.requested_reviewers.length}`)
+
+  if (pr.requested_reviewers.length > 0) {
+    core.info('Some reviewers are still in review.')
+    return false
+  }
+
+  const octokit = github.getOctokit(token)
+  const {data: reviews} = await octokit.rest.pulls.listReviews({
+    ...github.context.repo,
+    pull_number: pr.number,
+    per_page: 100 // TODO: over 100
+  })
+
+  core.debug(`reviews: ${reviews.length}`)
+
+  if (reviews.length == 0) {
+    core.info('There is no reviewers.')
+    return false
+  }
+
+  let latestReviews = reviews
+    .reverse()
+    .filter(review => review.user?.id != pr.user.id)
+    .filter(review => review.state.toLowerCase() != 'commented')
+    .filter((review, index, array) => {
+      // unique
+      return array.findIndex(x => review.user?.id === x.user?.id) === index
+    })
+
+  latestReviews.forEach(review => {
+    core.debug(`${review.user?.login} is ${review.state.toLowerCase()}.`)
+  })
+
+  if (
+    !latestReviews.every(review => review.state.toLowerCase() == 'approved')
+  ) {
+    core.info('Some reviewers do not approve.')
+    return false
+  }
+
+  core.info('All reviewers approve.')
+  return true
+}
--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,20 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import {
-  PullRequestEvent,
-  PullRequestReviewEvent
-} from '@octokit/webhooks-definitions/schema'
+import {approved} from './approved'
 
 async function run(): Promise<void> {
   try {
     const token: string = core.getInput('token')
 
-    const {pull_request: pr} =
-      github.context.eventName == 'pull_request'
-        ? (github.context.payload as PullRequestEvent)
-        : (github.context.payload as PullRequestReviewEvent)
-
-    core.debug(`PR#${pr.number}`)
-    core.debug(`requested reviewers: ${pr.requested_reviewers.length}`)
-
-    if (pr.requested_reviewers.length > 0) {
-      throw new Error('Some reviewers are still in review.')
-    }
+    core.debug(JSON.stringify(github.context))
 
     const octokit = github.getOctokit(token)
-    const {data: reviews} = await octokit.rest.pulls.listReviews({
+    await octokit.rest.repos.createCommitStatus({
       ...github.context.repo,
-      pull_number: pr.number,
-      per_page: 100 // TODO: over 100
+      sha: github.context.sha,
+      state: (await approved(token)) ? 'success' : 'failure',
+      context: 'review passing'
     })
-
-    core.debug(`reviews: ${reviews.length}`)
-
-    if (reviews.length == 0) {
-      throw new Error('There is no reviewers.')
-    }
-
-    let latestReviews = reviews
-      .reverse()
-      .filter(review => review.user?.id != pr.user.id)
-      .filter(review => review.state.toLowerCase() != 'commented')
-      .filter((review, index, array) => {
-        // unique
-        return array.findIndex(x => review.user?.id === x.user?.id) === index
-      })
-
-    latestReviews.forEach(review => {
-      core.debug(`${review.user?.login} is ${review.state.toLowerCase()}.`)
-    })
-
-    if (
-      !latestReviews.every(review => review.state.toLowerCase() == 'approved')
-    ) {
-      throw new Error('Some reviewers do not approve.')
-    }
-
-    core.debug('All reviewers approve.')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
